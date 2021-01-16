a13f147f4a97d2c4ca567dbaa351643db019d4c2
LukeSutor <64111222+LukeSutor@users.noreply.github.com>
2021-01-15 22:55:06 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ ReactDOM.render(
   domain="dev-hcy8y0so.us.auth0.com"
   clientId="CBBTcAiRUoGsp5D2Yb8eF8tntjKR3NCL"
   // Use http://localhost:3000/ when testing code, use https://the-stat-sheet.herokuapp.com/ for production
-  redirectUri="http://localhost:3000/">
+  redirectUri="https://the-stat-sheet.herokuapp.com/">
     <App />
   </Auth0Provider>,
   document.getElementById('root')
