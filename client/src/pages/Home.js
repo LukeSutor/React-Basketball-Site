f9df59e58f1002123ccb021c72accfa6287a8327
LukeSutor <lukesutor@gmail.com>
2021-02-13 18:37:50 -0500

    new landing page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react'
-import basketball_background from './images/basketball_background_1.png'
 import { useAuth0 } from '@auth0/auth0-react'
 import { useTransition, animated } from 'react-spring'
+import BlueBasketballSVG from './images/BlueBasketballSVG'
+import iPhone from './images/iPhone.png'
+import Graph from './images/Graph'
+import Record from './images/Record'
+import Share from './images/Share'
 
 function Home() {
 
@@ -20,23 +24,34 @@ function Home() {
 
   return (
     <div>
-      <div className="bg-main w-full md:h-3/4 mt-8 text-white">
-        <div className="flex flex-col-reverse md:flex-row justify-around">
-          {transition.map(({ item, key, props }) =>
-            item &&
-            <animated.div key={key} style={props} className="flex flex-col justify-evenly w-3/4 md:w-1/3 text-center md:text-left mx-auto">
-              <p className="text-4xl md:text-5xl lg:text-7xl font-semibold md:-mt-6">Statbreak</p>
-              <p className="text-md md:text-xl lg:text-2xl md:-mt-4 lg:-mt-8 py-8 md:py-0">Record your stats, track your progress, share with your coaches and peers.</p>
-              <div className="bg-white text-center w-min mx-auto rounded-full whitespace-nowrap hover:bg-gray-100 mb-4 md:mb-0">
-                <button className={`text-main text-md md:text-lg font-semibold px-2 md:px-4 py-1 md:py-2 focus:outline-none
+      <BlueBasketballSVG />
+      <div className="flex flex-row justify-evenly">
+        <div className="w-full md:w-3/5 mx-auto">
+          <div className="mx-auto text-center">
+            <h1 className="text-6xl md:text-7xl lg:text-8xl font-semibold my-20 md:my-8">Statbreak</h1>
+            <div className="flex flex-row flex-wrap justify-around text-md md:text-xl">
+              <div className="flex flex-col w-28 md:w-40 lg:w-44">
+                <Record />
+                <p className="my-2">Record your <br /> stats</p>
+              </div>
+              <div className="flex flex-col w-28 md:w-40  lg:w-44">
+                <Graph />
+                <p className="my-2">Track your <br /> progress</p>
+              </div>
+              <div className="flex flex-col w-28 md:w-40 lg:w-44">
+                <Share />
+                <p className="my-2">Share with your <br /> coaches and peers</p>
+              </div>
+            </div>
+            <div className="bg-main text-center w-min mx-auto rounded-full whitespace-nowrap hover:bg-dark mt-14 md:mt-8">
+                <button className={`text-white text-lg md:text-lg font-semibold px-4 py-2 focus:outline-none
               ${isAuthenticated ? "hidden" : ""}`}
                   onClick={() => loginWithRedirect()}>Get Started</button>
               </div>
-            </animated.div>
-          )}
-          <div className="w-3/4 md:w-1/2 py-6 md:py-0n mx-auto my-auto">
-            <img src={basketball_background} alt="basketball vector" />
           </div>
+        </div>
+        <div className="relative hidden md:flex z-20 w-1/3 mt-8">
+          <img src={iPhone} alt="" className="mx-auto my-auto" style={{ maxWidth: "12em" }}/>
         </div>
       </div>
     </div>
